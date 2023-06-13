b5fea0c6cfb123d1c42bfdf8647bf8e3131f8357
Alejandro Valdivia <alejandrovaldivia1@upb.edu>
2023-06-13 00:10:39 -0700

    Movimiento de ruedas con joints

--- Assets/Conducir.cs
+++ Assets/Conducir.cs
@@ -4,22 +4,60 @@ using UnityEngine;
 
 public class Conducir : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D RuedaDelanteraRB;
-    [SerializeField] private Rigidbody2D RuedaTraceraRB;
-    [SerializeField] private float velocidad = 150f;
+    public WheelJoint2D ruedaDelantera;
+    public WheelJoint2D ruedaTrasera;
+    public float velocidad = 650;
 
-    private float movimiento;
+    private Coroutine reduccionCorutina;
 
     // Start is called before the first frame update
     void Start()
     {
-        movimiento = Input.GetAxisRaw("Horizontal");
+        if (ruedaDelantera == null)
+            ruedaDelantera = GetComponentInChildren<WheelJoint2D>();
+
+        if (ruedaTrasera == null)
+            ruedaTrasera = GetComponentInChildren<WheelJoint2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        RuedaDelanteraRB.AddTorque(-movimiento * velocidad * Time.fixedDeltaTime);
-        RuedaTraceraRB.AddTorque(-movimiento * velocidad * Time.fixedDeltaTime);
+        //Control de la flecha izquierda
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            CambiarVelocidadRueda(ruedaDelantera, velocidad);
+            CambiarVelocidadRueda(ruedaTrasera, velocidad);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            ruedaDelantera.useMotor = false;
+            ruedaTrasera.useMotor = false;
+            //CambiarVelocidadRueda(ruedaDelantera, 0f);
+            //CambiarVelocidadRueda(ruedaTrasera, 0f);
+        }
+
+        //Control de la flecha derecha
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            CambiarVelocidadRueda(ruedaDelantera, -velocidad);
+            CambiarVelocidadRueda(ruedaTrasera, -velocidad);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            ruedaDelantera.useMotor = false;
+            ruedaTrasera.useMotor = false;
+            //CambiarVelocidadRueda(ruedaDelantera, 0f);
+            //CambiarVelocidadRueda(ruedaTrasera, 0f);
+        }
+    }
+
+    private void CambiarVelocidadRueda(WheelJoint2D rueda, float nuevaVelocidad)
+    {
+        JointMotor2D motor = rueda.motor;
+        motor.motorSpeed = nuevaVelocidad;
+        rueda.motor = motor;
     }
 }
